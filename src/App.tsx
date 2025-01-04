98df41bb996df8660d3c4a77bbf4e6a1207f81c2
ohhyunji <cuteohj44@gmail.com>
2025-01-05 00:17:52 +0900

    로컬 port 설정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to my Portfolio</h1>
+      <h1>오현지의 프로젝트</h1>
+      <p>오늘의 환경세팅 정도만...</p>
     </div>
   );
 }
